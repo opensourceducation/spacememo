cfd7a417f3ac7901bd252a51e3bf03e9b872de58
jsvanilla <36719905+jsvanilla@users.noreply.github.com>
2022-12-27 05:12:37 -0600

    CHANGES yml pipelines

--- javascript/semantic-release.config.js
+++ javascript/semantic-release.config.js
@@ -6,4 +6,9 @@ module.exports = {
     '@semantic-release/npm',
     '@semantic-release/github',
   ],
+  // Añade esta opción para que "semantic-release" utilice la variable de entorno GITHUB_TOKEN para autenticarse con GitHub
+  github: {
+    type: 'oauth',
+    token: process.env.GITHUB_TOKEN,
+  },
 };
